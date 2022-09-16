e46e11398994ab24d8f7a427c32015ebee5c4377
Msahu201989 <msahu201989@gmail.com>
2022-09-16 19:43:06 +0530

    Catalogue If else added

--- catalogue.sh
+++ catalogue.sh
@@ -2,42 +2,79 @@ LOG_FILE=/tmp/catalogue
 
 echo "Setup Nodejs Repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
+if [ $? -eq 0 ]; then
+  echo Status = Success
+  else
+   echo Status = Failure
+   fi
 echo status = $?
 
 echo "Installing NodeJs"
 yum install nodejs -y &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+  else
+   echo Status = Failure
+   fi
 
 echo "Creating user Roboshop"
 useradd roboshop &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+  else
+   echo Status = Failure
+   fi
 
 echo "Downloding Catalogue Application code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+  else
+   echo Status = Failure
+   fi
 
 cd /home/roboshop
 
 echo "Extracting Catalogue Application code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+  else
+   echo Status = Failure
+   fi
 
 echo "Moving Files to Catalogue"
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+  else
+   echo Status = Failure
+   fi
 
 echo "Installing NodeJs Dependencies"
 npm install &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+  else
+   echo Status = Failure
+   fi
 
 echo "Setup Catalogue service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+  else
+   echo Status = Failure
+   fi
 
 systemctl daemon-reload &>>${LOG_FILE}
 systemctl enable catalogue &>>${LOG_FILE}
 
 echo "start catalogue service"
 systemctl start catalogue &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = Success
+  else
+   echo Status = Failure
+   fi
