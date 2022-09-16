5b5fdbce922d2e065da89a091cc0c9910ed95a7e
Msahu201989 <msahu201989@gmail.com>
2022-09-16 13:50:19 +0530

    Mongodb

--- Mongodb.sh
+++ Mongodb.sh
@@ -8,6 +8,10 @@ echo "Installing MongoDB Repo"
 yum install -y mongodb-org &>>$LOG_FILE
 echo status = $?
 
+echo "update mongodb Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+echo status = $?
+
 echo "Startiong mongodb service"
 systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
