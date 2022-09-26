8ed3e6f878c502e67faca2891bf140ffa4759f48
Msahu201989 <msahu201989@gmail.com>
2022-09-26 19:55:18 +0530

    fronend test 2

--- frontest2.sh
+++ frontest2.sh
@@ -1,7 +1,14 @@
-
-yum list all | grep nginx
+echo Nginx installation in progress ......
+yum list all | grep nginx &>>/tmp/frontend
+echo status = $?
 yum install nginx -y &>>/tmp/frontend
+echo status = $?
+echo installation going on
 systemctl enable nginx
+echo enabling nginx
+echo status = $?
 systemctl start nginx
+echo starting nginx
+echo status = $?
 
 echo installation completed
